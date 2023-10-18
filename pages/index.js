941106d3f3cf58455a624f91b5cd540e843883d2
mryan-3 <malawaryan@gmail.com>
2023-10-18 12:35:57 +0300

    quot

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
               Backend Developer
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-            &quot;Results-driven backend developer focused on delivering reliable and functional code solutions. With a proactive and problem-solving mindset, I am dedicated to meeting project goals and ensuring the successful implementation of your requirements. Let's collaborate and create something exceptional.&quot;
+            &quot;Results-driven backend developer focused on delivering reliable and functional code solutions. With a proactive and problem-solving mindset, I am dedicated to meeting project goals and ensuring the successful implementation of your requirements. Let&apos;s collaborate and create something exceptional.&quot;
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <AiFillTwitterCircle />
@@ -222,7 +222,7 @@ export default function Home() {
             >
             <div className="flex flex-col justify-end h-full p-4 mt-4 ">
               <p className="text-lg font-bold">AI Hub</p>
-              <p className="text-md py-2"> &quot;A dynamic project combining Node.js, Express, and the Africa's Talking API to enable SMS communication from your application with just a prompt, inspired by the chat-based GPT AI model.&quot;</p>
+              <p className="text-md py-2"> &quot;A dynamic project combining Node.js, Express, and the Africa&apos;s Talking API to enable SMS communication from your application with just a prompt, inspired by the chat-based GPT AI model.&quot;</p>
               <a
                 href="https://github.com/mryan-3/smsRoute.git"
                 target="_blank"
